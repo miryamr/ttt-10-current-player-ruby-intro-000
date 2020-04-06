43cfd574053b08f520a46374ca771fd17c4bab28
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-06 20:52:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,13 +12,6 @@ counter = 0
 if my_condition_is_met
   counter += 1
 end
-#it 'counts occupied positions' do
-#how many turns have been played
-#return number of turns played
-#how will we determine whose turn it is?
-#IF
-#what do i want to DO with each element, with "X" and "O"
-#how many turns have been taken? by how many positions available there are on the board...
-#needs to return a NUMBER
-#number of turns
+
 #to loop over the elements of the board array.
+# of turns with counter 
