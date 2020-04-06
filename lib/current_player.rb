4216ea272b6a9ac01cdd97b9c9631e15d9d5a1b2
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-06 21:12:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,10 +5,7 @@ def turn_count(board)
 
 end
 
-#brothers = ["Tim", "Tom", "Jim"]
-#brothers.each do |brother|
-#puts "Stop hitting yourself #{brother}!"
-#end
+
 #to loop over the elements of the board array.
 # of turns with counter
 #need to get count to count the X's and O's on the board
