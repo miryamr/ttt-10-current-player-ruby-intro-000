908c13e4ddc896a487c2770baff91fbf558422c3
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-06 20:47:54 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+board = ["X", "O"]
+count = 0
+def turn_count(board)
+  board.each do |turn|
+      puts "#{turn}"
+      count += 1
+    end
+
+end
+
+
+#it 'counts occupied positions' do
+#how many turns have been played
+#return number of turns played
+#how will we determine whose turn it is?
+#IF
+#what do i want to DO with each element, with "X" and "O"
+#how many turns have been taken? by how many positions available there are on the board...
+#needs to return a NUMBER
+#number of turns
+#to loop over the elements of the board array.
