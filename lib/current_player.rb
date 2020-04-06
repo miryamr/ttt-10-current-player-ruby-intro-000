e73f68083bc67e9a491750c32f89a93d8aacdd13
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-06 21:55:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,13 @@
 require "pry"
-# player = ["X", "O"]
-def turn_count(board)
-  counter = 0
-  board.each do |board_index|
-    if board_index == "X" || board_index == "O"
-    counter += 1
+
+def turn_count(board) #pulling the argument from the spec file 
+  counter = 0 #setting the counter to 0 - that's what we use to return a number 
+  board.each do |board_index| #looping, "renaming" the board to board_index ie. the space and it's corresponding number on the board 
+    if board_index == "X" || board_index == "O" # here we set the condition for counter saying, if the space on the board has a value of X or O ...
+    counter += 1 #than, add 1
     end
   end
-  return counter
+  return counter #here we say give us a number at the end 
 end
 
 # if a token on the board is filled with and X or an O we count it
