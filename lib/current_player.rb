c1eed8b80ef764b579f369d8e6327a2388210fa8
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-06 20:54:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,17 @@
 board = ["X", "O"]
 counter = 0
-def turn_count(board)
-  board.each do |turn|
-      puts "#{turn}"
-      count += 1
-    end
-
+board.each do |turn|
+  puts "#{turn}"
+  if board 
 end
 
 counter = 0
 if my_condition_is_met
   counter += 1
 end
-
+#brothers = ["Tim", "Tom", "Jim"]
+#brothers.each do |brother|
+#puts "Stop hitting yourself #{brother}!"
+#end
 #to loop over the elements of the board array.
 # of turns with counter
